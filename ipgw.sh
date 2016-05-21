7e53fcfcfafa0bb89271b70f01f4b1838ed413dc
LI JIAHAO <lijiahao99131@gmail.com>
2016-05-21 14:06:48 +0800

    bug on loading source
    By this change you can exec it without cd to its root

--- ipgw.sh
+++ ipgw.sh
@@ -7,7 +7,15 @@
 #########################################################################
 #!/bin/bash
 
-source ./user.cfg
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do # resolve $SOURCE until the file is no longer a symlink
+	    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+		SOURCE="$(readlink "$SOURCE")"
+		[[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+
+source $DIR/user.cfg
 
 LOGOUT=`curl -s -d "action=logout&ac_id=1&user_ip=&nas_ip=&user_mac=&url=&username=$USER_NAME&password=$USER_PASS&save_me=0" "http://ipgw.neu.edu.cn:803/srun_portal_pc.php?ac_id=1&" 2>&1`
 LOGIN=`curl -s -d "action=login&ac_id=1&user_ip=&nas_ip=&user_mac=&url=&username=$USER_NAME&password=$USER_PASS&save_me=0" "http://ipgw.neu.edu.cn:803/srun_portal_pc.php?ac_id=1&" 2>&1`
