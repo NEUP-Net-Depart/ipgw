209a4358243076aaaf8b869228e3af1c8fd0f59f
LI JIAHAO <lijiahao99131@gmail.com>
2016-05-21 19:07:11 +0800

    big bug
    上一个传错了……

--- install.sh
+++ install.sh
@@ -7,9 +7,31 @@
 #########################################################################
 #!/bin/bash
 
+echo "欢迎使用neu ipgw登陆器 2645工作室出品"
+echo "请输入校园网账号："
+read username
+echo "请输入校园网密码："
+stty -echo
+read password
+echo "请再次输入校园网密码："
+read repassword
+
+while [ $password != $repassword ];
+do
+echo "两次输入的密码不一致，请重试！"
+echo "请输入校园网密码："
+read password
+echo "请再次输入校园网密码："
+read repassword
+done
+stty echo
+
 mkdir /usr/local/neuipgw
 cp ./ipgw.sh /usr/local/neuipgw/ipgw.sh
-cp ./user.cfg /usr/local/neuipgw/user.cfg
+#cp ./user.cfg /usr/local/neuipgw/user.cfg
+echo "#!/bin/bash" > /usr/local/neuipgw/user.cfg
+echo "USER_NAME=$username" >> /usr/local/neuipgw/user.cfg
+echo "USER_PASS=$password" >> /usr/local/neuipgw/user.cfg
 cp ./ipgw.png /usr/local/neuipgw/ipgw.png
 
 echo [Desktop Entry]>/usr/share/applications/neuipgw.desktop
@@ -20,3 +42,7 @@ echo Icon=/usr/local/neuipgw/ipgw.png>>/usr/share/applications/neuipgw.desktop
 echo Terminal=true>>/usr/share/applications/neuipgw.desktop
 echo Type=Application>>/usr/share/applications/neuipgw.desktop
 echo Categories=Network>>/usr/share/applications/neuipgw.desktop
+
+echo "安装成功！"
+
+
