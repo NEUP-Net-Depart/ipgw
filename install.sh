aadba0340cceb7d728b574e7a97239f0958b01f1
LI JIAHAO <lijiahao99131@gmail.com>
2016-05-21 14:39:00 +0800

    installer

---
+++ install.sh
@@ -0,0 +1,22 @@
+#########################################################################
+# File Name: install.sh
+# Author: ##########
+# ###############
+# mail: #################
+# Created Time: Sat 21 May 2016 02:18:25 PM CST
+#########################################################################
+#!/bin/bash
+
+mkdir /usr/local/neuipgw
+cp ./ipgw.sh /usr/local/neuipgw/ipgw.sh
+cp ./user.cfg /usr/local/neuipgw/user.cfg
+cp ./ipgw.png /usr/local/neuipgw/ipgw.png
+
+echo [Desktop Entry]>/usr/share/applications/neuipgw.desktop
+echo Name=NEU-IPGW>>/usr/share/applications/neuipgw.desktop
+echo Comment=Speedier Internet Access>>/usr/share/applications/neuipgw.desktop
+echo Exec=sh /usr/local/neuipgw/ipgw.sh>>/usr/share/applications/neuipgw.desktop
+echo Icon=/usr/local/neuipgw/ipgw.png>>/usr/share/applications/neuipgw.desktop
+echo Terminal=true>>/usr/share/applications/neuipgw.desktop
+echo Type=Application>>/usr/share/applications/neuipgw.desktop
+echo Categories=Application>>/usr/share/applications/neuipgw.desktop
